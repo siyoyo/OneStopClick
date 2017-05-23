8e6916a0f0039050e941b1bdd294d25b2ddb2e2a
MTMAC21 <mtmac21@mtmac21s-mini.mitrais.com>
2017-05-23 09:00:05 +0700

    merge

---
+++ app/Api/ProductService.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const APIManager = require('./APIManager');
+const ErrorMessages = require('../Util/ErrorMessages');
+const _ = require("underscore");
+const Rx = require('rx');
+
+const ProductService = Object.assign({}, APIManager, {
+    getIndex: function () {
+        var url = this.constructUrl('api/product');
+        const options = this.setupOptionsAuth("get");
+        return fetch(url, options)
+            .then(this.checkResponse)
+            .then(this.json)
+            .catch(response => {
+                return Promise.reject(ErrorMessages.serverError);
+            });
+    }
+
+});
+
+module.exports = ProductService;
